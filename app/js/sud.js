7910fe8e2b1cc50dab9341a785eee82f6aebd63b
Andrey Sirotkin <a.sirotkin@auvix.ru>
2019-08-27 19:06:34 +0300

    js sudocu solve

--- app/js/sud.js
+++ app/js/sud.js
@@ -11,21 +11,21 @@ var iii    = 0;
 function setEmpty()
 {
     number = "";
-    $("#" + idCell).html(number);
-    $("#" + idCell).attr("bgcolor", "black");
+    jQuery("#" + idCell).html(number);
+    jQuery("#" + idCell).attr("bgcolor", "black");
 }
 
 function setCell(_this)
 {
     idCell = _this.id;
-    $("#" + idCell).attr("bgcolor", "green");
+    jQuery("#" + idCell).attr("bgcolor", "green");
 }
 
 function setNumber(_this)
 {
     number = jQuery(_this).html();
-    $("#" + idCell).html(number);
-    $("#" + idCell).attr("bgcolor", "black");
+    jQuery("#" + idCell).html(number);
+    jQuery("#" + idCell).attr("bgcolor", "black");
 }
 
 function putNumber(_this)
@@ -46,6 +46,7 @@ function solveAll()
 {
     var rows = [];
     var val;
+    var s = "";
     for (var r = 0; r < 9; r ++) {
         rows[r] = [];
         for (var c = 0; c < 9; c ++) {
@@ -54,10 +55,11 @@ function solveAll()
                 val = 0;
             }
             rows[r][c] = val;
+            s = s + "" + val;
         }
     }
-    json = objAjaxJson("sud", "solve", "rows=" + rows);
-//    json = false;
+//    json = objAjaxJson("sud", "solve", "rows=" + rows);
+    json = false;
     if (json) {
         jQuery.each(json.rows, function(r, row) {
             jQuery.each(row, function(c, val) {
@@ -84,7 +86,14 @@ function solveAll()
         return;
     }
 
-    solveHelper(rows);
+    var time_beg = new Date().getTime();
+    var solver   = new SudokuSolver();
+
+    rows = solver.solve(s, { result: "chunks" });
+
+    var time = (new Date().getTime() - time_beg) / 1000.0;
+
+    jQuery("#time").html(time);
 
     jQuery.each(rows, function(r, row) {
         jQuery.each(row, function(c, val) {
@@ -98,6 +107,151 @@ function solveAll()
         });
     });
 }
+
+function SudokuSolver()
+{
+
+    var puzzle_table = [];
+    /*
+    * Check if the number is a legal candidate
+    * for the given cell (by Sudoku rules).
+    */
+    function check_candidate(num, row, col)
+    {
+        for (var i = 0; i < 9; i++) {
+            var b_index = ((Math.floor(row / 3) * 3) + Math.floor(i / 3)) * 9 + (Math.floor(col / 3) * 3) + (i % 3);
+            if (num == puzzle_table[(row * 9) + i] || num == puzzle_table[col + (i * 9)] || num == puzzle_table[b_index]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    /*
+    * Recursively test all possible numbers for a given cell until
+    * the puzzle is solved.
+    */
+    function get_candidate(index)
+    {
+        if (index >= puzzle_table.length) {
+            return true;
+        } else if (puzzle_table[index] != 0) {
+            return get_candidate(index + 1);
+        }
+
+        for (var i = 1; i <= 9; i++) {
+            if (check_candidate(i, Math.floor(index / 9), index % 9)) {
+                puzzle_table[index] = i;
+                if (get_candidate(index + 1)) {
+                    return true;
+                }
+            }
+        }
+
+        puzzle_table[index] = 0;
+        return false;
+    }
+    /*
+    * Split result of puzzle into chunks by 9.
+    */
+    function chunk_in_groups(arr) {
+        var result = [];
+        for (var i = 0; i < arr.length; i += 9) {
+            result.push(arr.slice(i, i + 9));
+        }
+        return result;
+    }
+    /*
+    * Start solving the game for provided puzzle and options.
+    */
+    this.solve = function (puzzle, options) {
+        options = options || {};
+        var result = options.result || 'string';
+        puzzle_table = puzzle.split('').map(function (v) {
+            return isNaN(v) ? 0 : +v
+        });
+    
+        if (puzzle.length !== 81) {
+            return 'Puzzle is not valid.'
+        }
+        return !get_candidate(0) ? 'No solution found.' : result === 'chunks' ? chunk_in_groups(puzzle_table) : result === 'array' ? puzzle_table : puzzle_table.join('');
+    }
+}
+
+/*
+function sudoku(puzzle)
+{
+    while (!isSolved(puzzle)) {
+        for (x = 0; x < 9; x++) {
+            for (y = 0; y < 9; y++) {
+                puzzle[y][x] = digit(puzzle, x, y);
+            }
+        }
+    }
+    return puzzle;
+}
+
+function digit(puzzle, x, y)
+{
+    if (puzzle[y][x] !== 0) return puzzle[y][x];
+
+    var row = puzzle[y];
+    var column = columnArray(puzzle, x);
+    var grid = gridArray(puzzle, x, y);
+
+    var knowns = row.concat(column, grid);
+
+    var possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9].filter(function(item) { return knowns.indexOf(item) === -1; });
+
+    return possibilities.length == 1 ? possibilities[0] : 0;
+}
+
+function columnArray(puzzle, idx)
+{
+    return puzzle.map(function(row) { return row[idx]; });
+}
+
+function gridArray(puzzle, x, y)
+{
+    x = Math.floor(x / 3) * 3;
+    y = Math.floor(y / 3) * 3;
+
+    var arr = [];
+
+    for (i = x; i < x + 3; i++) {
+        for (j = y; j < y + 3; j++) {
+            arr.push(puzzle[j][i]);
+        }
+    }
+
+    return arr;
+}
+
+function sum(arr)
+{
+    return arr.reduce(function(a, n) { return a + n; }, 0);
+}
+
+function winningRow(arr)
+{
+    return sum(arr.map(function(num) { return Math.pow(2, num - 1); })) == 511;
+}
+
+function isSolved(puzzle)
+{
+    return puzzle.filter(function (row) { return !winningRow(row); }).length === 0;
+}
+
+
+
+
+
+
+
+
+/*
+
+
+
 
 function solveHelper(solution)
 {
@@ -227,3 +381,4 @@ function array_diff(a1, a2)
     return diff;
 }
 
+*/
