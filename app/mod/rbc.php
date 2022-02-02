212d93e2b6b2756954e8ff8bbec8c6e71159a033
sro production <sro@sro-abc.ru>
2022-02-02 10:18:37 +0300

    from production

--- app/mod/rbc.php
+++ app/mod/rbc.php
@@ -10,28 +10,28 @@ require_once(MOD_DIR . "main.php");
 class rbc extends main
 {
 
-    public function show($params = array ())
+    public function show($params = [])
     {
-        $this->data = array (
-            "all" => array (
+        $this->data = [
+            "all" => [
                 "rows" => $this->storage->queryRows(
-                    "SELECT DATE_FORMAT(created, '%Y-%m-%d') AS `date`, AVG(USD) AS USD, AVG(brand) AS brand" .
-                    "  FROM rbc" .
-                    " WHERE USD <> 0" .
-                    "   AND brand <> 0" .
+                    "SELECT DATE_FORMAT(`created`, '%Y-%m-%d') AS `date`, AVG(`USD`) AS `USD`, AVG(`brand`) AS `brand`" .
+                    "  FROM `rbc`" .
+                    " WHERE `USD` <> 0" .
+                    "   AND `brand` <> 0" .
                     " GROUP BY 1" .
                     " ORDER BY 1 ASC"
                 ),
                 "min" => 20,
-                "divide" => 2500,
-            ),
-            "last" => array (
+                "divide" => 2000,
+            ],
+            "last" => [
                 "rows" => array_reverse(
                     $this->storage->queryRows(
-                        "SELECT DATE_FORMAT(created, '%Y-%m-%d') AS `date`, AVG(USD) AS USD, AVG(brand) AS brand" .
-                        "  FROM rbc" .
-                        " WHERE USD <> 0" .
-                        "   AND brand <> 0" .
+                        "SELECT DATE_FORMAT(`created`, '%Y-%m-%d') AS `date`, AVG(`USD`) AS `USD`, AVG(`brand`) AS `brand`" .
+                        "  FROM `rbc`" .
+                        " WHERE `USD` <> 0" .
+                        "   AND `brand` <> 0" .
                         " GROUP BY 1" .
                         " ORDER BY 1 DESC" .
                         " LIMIT 500"
@@ -39,23 +39,24 @@ class rbc extends main
                 ),
                 "min" => 35,
                 "divide" => 3000,
-            ),
-            "cur" =>  array (
+            ],
+            "cur" =>  [
                 "rows" => array_reverse(
                     $this->storage->queryRows(
-                        "SELECT DATE_FORMAT(created, '%Y-%m-%d %H:30:00') AS `date`, AVG(USD) AS USD, AVG(brand) AS brand" .
-                        "  FROM rbc" .
-                        " WHERE USD <> 0" .
-                        "   AND brand <> 0" .
+                        "SELECT DATE_FORMAT(`created`, '%Y-%m-%d %H:30:00') AS `date`, AVG(`USD`) AS `USD`, AVG(`brand`) AS `brand`" .
+                        "  FROM `rbc`" .
+                        " WHERE `USD` <> 0" .
+                        "   AND `brand` <> 0" .
                         " GROUP BY 1" .
                         " ORDER BY 1 DESC" .
-                        " LIMIT 500"
+                        " LIMIT 85"
                     )
                 ),
                 "min" => 45,
                 "divide" => 0,
-            ),
-        );
-        $this->data["cur"]["divide"] = empty($_REQUEST["divide"]) ? round($this->data["cur"]["rows"][count($this->data["cur"]["rows"]) - 1]["USD"] * $this->data["cur"]["rows"][count($this->data["cur"]["rows"]) - 1]["brand"] / 50) * 50 : $_REQUEST["divide"];
+            ],
+        ];
+        $this->data["cur"]["divide"] = empty($_REQUEST["divide"]) ? round($this->data["cur"]["rows"][count($this->data["cur"]["rows"]) - 1]["USD"] * $this->data["cur"]["rows"][count($this->data["cur"]["rows"]) - 1]["brand"] / 10) * 10 : $_REQUEST["divide"];
+//        $this->data["cur"]["divide"] = empty($_REQUEST["divide"]) ? round($this->data["cur"]["rows"][count($this->data["cur"]["rows"]) - 1]["USD"] * $this->data["cur"]["rows"][count($this->data["cur"]["rows"]) - 1]["USD"] * $this->data["cur"]["rows"][count($this->data["cur"]["rows"]) - 1]["brand"] / 50) * 50 : $_REQUEST["divide"];
     }
 }
